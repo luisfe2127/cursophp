766a258dad49b4a9c46214ac4365ff7c50e53c9f
Luís Felipe <luis.canarinho2126@gmail.com>
2024-10-18 23:14:51 -0300

    atualizando ex003 (tipos primitivos)

---
+++ ex003/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="t-brp">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos</title>
+</head>
+
+<body>
+    <h1>Teste de tipos Primitivos</h1>
+
+    <?php 
+    
+    // 0x = hexadecimal 0b = binario 0 = octal
+    
+    // $num = 0x20;
+    
+    // echo "o valor da variavel é $num";
+
+    //  $v = 90;
+
+    // var_dump($v);
+
+    // $num = 3e2; // 3x10^2
+    // echo "o valor da variavel é $num";
+
+    // $num = (int) "950"; //coerçao
+    // var_dump($num);
+
+    $vet = [2,4,6,8];
+    var_dump($vet);
+
+?>
+
+</body>
+
+</html>
