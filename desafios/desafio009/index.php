8ecd1283da8bb70ce81d67e9a7aa5b06544135c5
Luís Felipe <luis.fe8000@gmail.com>
2024-11-28 14:45:07 -0300

    criando desafio007,desafio008 (calculando raiz quadrada e cubica) e desafio009

---
+++ desafios/desafio009/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Média Aritimetica</title>
+</head>
+<body>
+
+<h1>Calculando Média e Mediana dos valores</h1>
+<section id="formulario">
+    <form action="index.php" method="get">
+        
+    </form>
+
+</section>
+    
+</body>
+</html>
